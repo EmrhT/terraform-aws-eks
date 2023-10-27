d0dc77254f0a01e2ef1585242293c9b7bc4951c3
emrht <emrhtfn@gmail.com>
2023-10-28 02:16:20 +0300

    eks module updated

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -37,8 +37,9 @@ locals {
 ################################################################################
 
 module "eks" {
-  source = "../.."
-
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+  
   cluster_name                   = local.name
   cluster_endpoint_public_access = true
 
